77d9546e6fecc09bad0cbf4a8a3777c985da1014
marcstreeter <mstreeter@gmail.com>
2016-12-13 11:04:41 -0600

    I finished lecture 58 section 6 I added some tasks and and  a exclamation point ❗️to show that a task is important

--- DoItRight!/ViewController.swift
+++ DoItRight!/ViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
-   
+   @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var tableView: UITableView!
-    
+    var tasks : [Task] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
+    tasks = makeTasks()
+        
     tableView.dataSource = self
     tableView.delegate = self
     
@@ -26,16 +27,39 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 10
+       return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "hello"
+        let task = tasks[indexPath.row]
+        if task.important {
+         cell.textLabel?.text = "❗️\(task.name)"
+        }else{
+          cell.textLabel?.text = task.name
+        }
+      
         return cell
     }
     
+    func makeTasks() -> [Task] {
+    let task1 = Task()
+    task1.name = "Learn Programing"
+    task1.important = true
     
+    let task2 = Task()
+    task2.name = "Do all my programing and finish DoiT"
+    task2.important = true
+        
+    let task3 = Task()
+    task3.name = "make sure my sister dosen't eat my dessert"
+    task3.important = false
+    
+    return [task1,task2,task3]
+        
+    
+    }
+
     
 }
 
---
+++ Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoItRight!
+//
+//  Created by Marc Streeter on 12/13/16.
+//  Copyright © 2016 Developer Warriors. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+var name = ""
+var important = false  
+}
